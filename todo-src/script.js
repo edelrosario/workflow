7a89b1c2b4a277d5b2727fe9fd0ad3bf9c7328c6
Elaine Del Rosario <e.delrosario94@gmail.com>
2015-09-11 22:52:11 -0400

    Removed the extra scrope.todo variable

--- todo-src/script.js
+++ todo-src/script.js
@@ -3,13 +3,13 @@
 var myApp = angular.module('app', []);
 
 myApp.controller('MainCtrl', function ($scope){
-  $scope.priorities = ["Now", "Tomorrow", "Someday"]
+  $scope.priorities = ["Now", "Tomorrow", "Someday"];
   $scope.todos = [
     {"text":"Learn Angular", "prio":"Now"},
     {"text":"Learn node", "prio": "Someday"}
   ];
   $scope.newItem = {};
-  $scope.todos = ["Learn Angular", "Learn node"];
+  //$scope.todos = ["Learn Angular", "Learn node"];
   $scope.newItem = "";
   $scope.totalItems = 2;
   
